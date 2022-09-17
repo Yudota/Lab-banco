14c78da3f8013a3bc7aa6eb1f300ca341f02e7d9
Yudota <yuri.fernandes.961@gmail.com>
2022-09-17 03:36:36 -0300

    queries para construção do user e estrutura tabelão

---
+++ criacao-users/createuser.sql
@@ -0,0 +1,8 @@
+-- ALTER SESSION
+alter session set "_ORACLE_SCRIPT" = true;
+-- USER SQL
+ create user app identified by app123 default tablespace users quota unlimited on users;
+ grant connect, resource to app;
+
+-- SYSTEM PRIVILEGES
+
